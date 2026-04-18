bfc47dbfcad85a2b28be0b8ca5595b943db5a0d8
qwert379 <ola.k119@o2.pl>
2026-04-18 18:29:54 +0000

    Dodanie klasy Employee

---
+++ src/main/java/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+    // Pola klasy (stan obiektu)
+    private String name;
+    private int skill;
+    private double salary;
+
+    // Konstruktor (tworzy nowego pracownika)
+    public Employee(String name, int skill, double salary) {
+        this.name = name;
+        this.skill = skill;
+        this.salary = salary;
+    }
+
+    // Metoda work (zachowanie obiektu)
+    public int work() {
+        return this.skill;
+    }
+}
